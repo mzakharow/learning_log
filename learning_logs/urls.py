d3f30eefe3d336085ec496e2f6ef65202bbf110a
mzakharow <zaharow@bk.ru>
2018-02-01 00:01:28 +0300

    list's pages and singl page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,4 +6,10 @@ app_name ='learning_logs'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    #List
+    url(r'^topics/$', views.topics, name='topics'),
+
+    #single
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
